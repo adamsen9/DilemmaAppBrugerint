c51bf57b292a971fa33d76f1180946d8dedd9c11
adamsen9 <adamsen9@gmail.com>
2015-12-11 02:41:28 +0100

    second commit

--- app/src/main/java/adamsen/dk/Dilemma40/DataProvider.java
+++ app/src/main/java/adamsen/dk/Dilemma40/DataProvider.java
@@ -12,15 +12,15 @@ public class DataProvider {
     public static ArrayList<Dilemma> getInfo() {
         ArrayList<Dilemma> Dilemmaer = new ArrayList<>();
 
-        String[] valg1 = {"1","2","3"};
+        String[] valg1 = {"The Roon","Jack and Gill","Birdemic"};
         String[] valg2 = {"Drop hende","Slå op med hende","Ignorer hende"};
-        String[] valg3 = {"Hej","Med","Dig","so"};
-        String[] valg4 = {"Hej","Med"};
+        String[] valg3 = {"Tag på arbejde","Meld dig syg","Bare lad være med at møde op","Melde mig ind i fremmedlegionen"};
+        String[] valg4 = {"Kjole 1","Kjole 2"};
 
-        Dilemmaer.add(new Dilemma("Hvilken film skal jeg se?","0",valg1));
+        Dilemmaer.add(new Dilemma("Hvilken film skal jeg se?","Jeg keder mig bravt og vil bare vildt gerne se en god film, dette er mit udvalg.",valg1));
         Dilemmaer.add(new Dilemma("Jeg hader min kæreste","Min kæreste er virkelig nederen lige nu, hvad gør jeg?",valg2));
-        Dilemmaer.add(new Dilemma("Gider ikke på arbejde","Robert",valg3));
-        Dilemmaer.add(new Dilemma("Min mor er en so","Swag",valg4));
+        Dilemmaer.add(new Dilemma("Gider ikke på arbejde","Jeg skal på arbejde men gider virkelig ikke, jeg hader min chef og er ret sikker på han også hader mig, hvad gør jeg?",valg3));
+        Dilemmaer.add(new Dilemma("Hvilken kjole skal jeg have på?","Jeg kan bare virkelig ikke beslutte mig for hvilken kjole jeg gerne vil have på!",valg4));
 
         return Dilemmaer;
     }
--- app/src/main/java/adamsen/dk/Dilemma40/MainActivity.java
+++ app/src/main/java/adamsen/dk/Dilemma40/MainActivity.java
@@ -3,6 +3,7 @@ package adamsen.dk.Dilemma40;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
+import android.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,15 +17,23 @@ public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Splash screen start
+
+
+        //Spash screen slut
+
+
 
         super.onCreate(savedInstanceState);
-
-        ArrayList<Dilemma> dilemmaer = new ArrayList<Dilemma>();
-
         setContentView(R.layout.activity_main);
+
+
+
         Exp_list = (ExpandableListView) findViewById(R.id.Listen);
         Dilemmaer = DataProvider.getInfo();
 
+
+        ArrayList<Dilemma> dilemmaer = new ArrayList<Dilemma>();
         adapter = new DilemmaAdapter(this, Dilemmaer);
         Exp_list.setAdapter(adapter);
     }
